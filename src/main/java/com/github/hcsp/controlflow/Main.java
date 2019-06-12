1c44af1aa0c66158532d5ce8b4e142933c289e47
JayChenFE <chenjiehwy@163.com>
2019-06-12 22:28:35 +0800

    质数 (#5)
    质数 (#5)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.controlflow;
 
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
         printPrimeNumbers();
@@ -10,5 +13,16 @@ public class Main {
      *
      * <p>提示：对于正整数n，如果用2到Math.sqrt(n)+1之间的所有整数去除，均无法整除，则n为质数。
      */
-    public static void printPrimeNumbers() {}
+    public static void printPrimeNumbers() {
+        final int number = 100;
+        boolean[] primeArr = new boolean[number + 1];
+        for (int i = 2; i <= number; i++) {
+            if (!primeArr[i]) {
+                System.out.printf(String.valueOf(i));
+                for (int j = i; j <= number; j += i) {
+                    primeArr[j] = true;
+                }
+            }
+        }
+    }
 }
