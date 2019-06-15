348e1a081fd321e9006606e35f84084554dc6841
Bo Zhang <bo@gradle.com>
2019-06-15 19:40:59 +0800

    Update repo

--- src/test/java/com/github/hcsp/controlflow/MainTest.java
+++ src/test/java/com/github/hcsp/controlflow/MainTest.java
@@ -3,6 +3,7 @@ package com.github.hcsp.controlflow;
 import com.github.blindpirate.extensions.CaptureSystemOutput;
 import java.util.stream.IntStream;
 import org.apache.commons.math3.primes.Primes;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
@@ -11,7 +12,7 @@ public class MainTest {
     public void importStringUtilsCorrectly(CaptureSystemOutput.OutputCapture capture) {
         IntStream.range(0, 100)
                 .filter(Primes::isPrime)
-                .forEach(i -> capture.expect(containsString("" + i)));
+                .forEach(i -> capture.expect(Matchers.containsString("" + i)));
         Main.printPrimeNumbers();
     }
 }
