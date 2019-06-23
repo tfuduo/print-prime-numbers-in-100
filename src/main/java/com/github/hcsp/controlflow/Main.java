50bcd921dd3973aafba656cb96a8994ba1b4322e
wanz <lzh60587@gmail.com>
2019-06-23 16:28:08 +0800

    print-prime-numbers-in-100

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -11,9 +11,10 @@ public class Main {
      * <p>提示：对于正整数n，如果用2到Math.sqrt(n)+1之间的所有整数去除，均无法整除，则n为质数。
      */
     public static void printPrimeNumbers() {
-        for(int i = 2; i<=100; i++) {
+        System.out.println(2);
+        for(int i = 3; i<=100; i++) {
             boolean flag = true;
-            for (int j = 2; j <Math.sqrt(i); j++) {
+            for (int j = 2; j <Math.sqrt(i) + 1; j++) {
                 if (i%j == 0) {
                     flag = false;
                     break;
