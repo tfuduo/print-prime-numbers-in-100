f6d4741fb8f3a3c11a3e19e5be8970db50acddb9
ZhangMingShuo <34833093+ZhangMingShuo@users.noreply.github.com>
2019-10-13 23:55:01 +0800

    controlflow:printPrime (#22)
    controlflow:printPrime (#22)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -4,11 +4,16 @@ public class Main {
     public static void main(String[] args) {
         printPrimeNumbers();
     }
-
-    /**
-     * 打印100以内的素数（质数）。 一个大于1的自然数，除了1和它自身外，不能被其他自然数整除的数叫做质数。
-     *
-     * <p>提示：对于正整数n，如果用2到Math.sqrt(n)+1之间的所有整数去除，均无法整除，则n为质数。
-     */
-    public static void printPrimeNumbers() {}
+    public static void printPrimeNumbers() {
+	System.out.println(2);
+        for(int i=2;i<100;i++){
+ 	    boolean isPr=true;
+	    for(int j=2;j<Math.sqrt(i)+1;j++){
+	        if(i%j==0)//is not prime
+		    {isPr=false;break;}
+	    }
+	    if(isPr)
+	        System.out.println(i);
+	}
+    }
 }
