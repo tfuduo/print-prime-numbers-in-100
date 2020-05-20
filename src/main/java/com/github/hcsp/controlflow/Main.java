5084833b83909d2ec68600bfcc7d69250059c84d
fuduo <329794799@qq.com>
2020-05-20 21:18:22 +0800

    Fix format.

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,7 +14,7 @@ public class Main {
         boolean isPrime = true;
         for (int i = 2; i <= 100; i++) {
             isPrime = true;
-            for (int j = 2; j <(int) (Math.sqrt(i) + 1); j++) {
+            for (int j = 2; j < (int) (Math.sqrt(i) + 1); j++) {
                 if (i % j == 0) {
                     isPrime = false;
                     break;
